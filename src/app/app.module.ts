b31a047c3baf412b17059cb9ed8ac2fb8d29e6f6
siv-engmark <siv.engmark@gmail.com>
2021-11-23 08:27:51 +0100

    Added dependencies - Firebase, Angular Material and Swiper

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'sustainability-info-site',
+    appId: '1:861691072232:web:2a82326ebb35c8cb53035d',
+    storageBucket: 'sustainability-info-site.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyC_pWNfG-zQ9XHT5VAj6c3Ezq7kD9LCN2s',
+    authDomain: 'sustainability-info-site.firebaseapp.com',
+    messagingSenderId: '861691072232',
+  },
   production: true
 };
